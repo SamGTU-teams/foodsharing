394647786b2f37e5539f402dd01b9463e97ffbbb
rassafel <rassafel@gmail.com>
2021-10-27 23:11:00 +0400

    Add maven build and docker build.

--- dev.sh
+++ dev.sh
@@ -1,3 +1,5 @@
 . ./env.sh
 
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+sh ./mvnw install -DskipTests
+
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
