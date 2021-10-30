59c5f7002244753651f0bcec78aebae616b54557
rassafel <rassafel@gmail.com>
2021-10-30 19:43:28 +0400

    Create bot-started and bots pom file.

---
+++ bots/bot-starter/src/main/java/ru/rassafel/bot/configuration/BotAutoConfiguration.java
@@ -0,0 +1,10 @@
+package ru.rassafel.bot.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author rassafel
+ */
+@Configuration
+public class BotAutoConfiguration {
+}
