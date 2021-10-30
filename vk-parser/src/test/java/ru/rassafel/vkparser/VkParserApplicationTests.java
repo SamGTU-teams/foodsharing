1763a72edc14106230e9986334d21a0700e029e4
rassafel <rassafel@gmail.com>
2021-10-30 17:05:15 +0400

    Create vk-parser module.

---
+++ vk-parser/src/test/java/ru/rassafel/vkparser/VkParserApplicationTests.java
@@ -0,0 +1,11 @@
+package ru.rassafel.vkparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkParserApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
