8eb9afbf42fe852d42ce3870494a0629bbbd25a8
rassafel <rassafel@gmail.com>
2022-01-04 20:19:04 +0400

    Add RabbitMQ configuration and simple listener for TG and VK bot`s.

---
+++ bots/tg-bot/src/main/java/ru/rassafel/foodsharing/tgbot/config/RabbitConfiguration.java
@@ -0,0 +1,57 @@
+package ru.rassafel.foodsharing.tgbot.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author rassafel
+ */
+@EnableRabbit
+@Configuration
+public class RabbitConfiguration {
+    @Value("${spring.rabbitmq.exchange}")
+    private String exchangeName;
+
+    @Value("${spring.rabbitmq.queue}")
+    private String queueName;
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return new FanoutExchange(exchangeName);
+    }
+
+    @Bean
+    public Queue tgQueue() {
+        return new Queue(queueName);
+    }
+
+    @Bean
+    public Binding tgBinding() {
+        return BindingBuilder.bind(tgQueue()).to(fanoutExchange());
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory,
+                                                                               SimpleRabbitListenerContainerFactoryConfigurer configurer) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        configurer.configure(factory, connectionFactory);
+        factory.setMessageConverter(jsonMessageConverter());
+        return factory;
+    }
+}
