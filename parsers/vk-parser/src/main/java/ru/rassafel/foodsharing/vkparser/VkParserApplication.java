d93875f6c20a27034404bfbe0704a9a67c31c58b
rassafel <rassafel@gmail.com>
2022-01-04 20:14:18 +0400

    Add EnableScheduling annotation.

--- parsers/vk-parser/src/main/java/ru/rassafel/foodsharing/vkparser/VkParserApplication.java
+++ parsers/vk-parser/src/main/java/ru/rassafel/foodsharing/vkparser/VkParserApplication.java
@@ -2,8 +2,10 @@ package ru.rassafel.foodsharing.vkparser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class VkParserApplication {
     public static void main(String[] args) {
         SpringApplication.run(VkParserApplication.class, args);
