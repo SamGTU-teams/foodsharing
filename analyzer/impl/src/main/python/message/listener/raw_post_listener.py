7811ae3b582df762868d7346452c32be1c381919
rassafel <rassafel@gmail.com>
2022-01-05 15:08:41 +0400

    Add publisher to listener.

--- analyzer/impl/src/main/python/message/listener/raw_post_listener.py
+++ analyzer/impl/src/main/python/message/listener/raw_post_listener.py
@@ -1,2 +1,8 @@
-def callback(ch, method, properties, body):
+import json
+
+from ..producer.post_producer import publish
+
+def callback(channel, method, properties, body):
     print(f"Incoming message: {body}")
+    data = json.loads(body)
+    publish(channel, data)
