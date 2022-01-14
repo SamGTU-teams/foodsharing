d97e96e030944638b66ec6d7b597697b1e3688c9
rassafel <rassafel@gmail.com>
2022-01-14 17:38:45 +0400

    Add auto configuration.

---
+++ platform/common-db/src/main/java/ru/rassafel/foodsharing/common/configuration/CommonAutoConfiguration.java
@@ -0,0 +1,10 @@
+package ru.rassafel.foodsharing.common.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author rassafel
+ */
+@Configuration
+public class CommonAutoConfiguration {
+}
