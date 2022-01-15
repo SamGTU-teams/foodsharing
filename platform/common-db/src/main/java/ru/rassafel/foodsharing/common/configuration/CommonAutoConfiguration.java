b332331cd49f8d45a4cff1e13da4555d39cf3930
rassafel <rassafel@gmail.com>
2022-01-15 19:35:58 +0400

    Add EntityScan annotation.

--- platform/common-db/src/main/java/ru/rassafel/foodsharing/common/configuration/CommonAutoConfiguration.java
+++ platform/common-db/src/main/java/ru/rassafel/foodsharing/common/configuration/CommonAutoConfiguration.java
@@ -1,10 +1,12 @@
 package ru.rassafel.foodsharing.common.configuration;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author rassafel
  */
 @Configuration
+@EntityScan
 public class CommonAutoConfiguration {
 }
