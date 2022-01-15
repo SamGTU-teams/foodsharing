3f285ae45fbaadb216888c8207baaacf61e9d4dd
rassafel <rassafel@gmail.com>
2022-01-15 20:36:44 +0400

    Create GeoPoint data class.

---
+++ platform/common/src/main/java/ru/rassafel/foodsharing/common/model/GeoPoint.java
@@ -0,0 +1,13 @@
+package ru.rassafel.foodsharing.common.model;
+
+import lombok.Data;
+
+/**
+ * @author rassafel
+ */
+@Data
+public class GeoPoint {
+    private double lat;
+
+    private double lon;
+}
