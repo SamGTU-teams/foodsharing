66c71e1a13cfde63f60978ed25de6a73863189e6
rassafel <rassafel@gmail.com>
2022-01-18 20:52:49 +0400

    Add AnalyzerController interface.

---
+++ analyzer/api/src/main/java/ru/rassafel/foodsharing/analyzer/controller/AnalyzerController.java
@@ -0,0 +1,11 @@
+package ru.rassafel.foodsharing.analyzer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author rassafel
+ */
+@RequestMapping(AnalyzerController.MAPPING)
+public interface AnalyzerController {
+    String MAPPING = "";
+}
