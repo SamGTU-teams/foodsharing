07d0af0375964f73a73063104d2edbda01c98e07
rassafel <rassafel@gmail.com>
2022-01-25 03:17:47 +0400

    Add implementation for callback endpoint.

---
+++ parsers/vk-parser/impl/src/main/java/ru/rassafel/foodsharing/vkparser/controller/impl/CallbackControllerImpl.java
@@ -0,0 +1,34 @@
+package ru.rassafel.foodsharing.vkparser.controller.impl;
+
+import com.vk.api.sdk.events.Events;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+import ru.rassafel.foodsharing.vkparser.controller.CallbackController;
+import ru.rassafel.foodsharing.vkparser.model.vk.CallbackMessage;
+import ru.rassafel.foodsharing.vkparser.service.CallbackService;
+
+/**
+ * @author rassafel
+ */
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+public class CallbackControllerImpl implements CallbackController {
+    private final CallbackService service;
+
+    @Override
+    public ResponseEntity<?> acceptUpdate(CallbackMessage message) {
+        log.info("Accepted message with type: {}", message.getType().name());
+        if (Events.CONFIRMATION.equals(message.getType())) {
+            String conformation = service.confirmation(message.getGroupId());
+            return ResponseEntity.ok(conformation);
+        }
+        if (Events.WALL_POST_NEW.equals(message.getType())) {
+            service.wallpostNew(message.getGroupId(), message.getWallpost(), message.getSecret());
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
+    }
+}
