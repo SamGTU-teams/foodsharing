1161a0a32b2c88083f69f563e1631bd15111d8ed
rassafel <rassafel@gmail.com>
2022-01-25 03:16:37 +0400

    Add endpoints for registration and callback.

---
+++ parsers/vk-parser/impl/src/main/java/ru/rassafel/foodsharing/vkparser/controller/GroupController.java
@@ -0,0 +1,25 @@
+package ru.rassafel.foodsharing.vkparser.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.rassafel.foodsharing.vkparser.model.vk.group.FullAccessGroup;
+import ru.rassafel.foodsharing.vkparser.model.vk.group.WithoutAccessGroup;
+
+import javax.validation.Valid;
+
+/**
+ * @author rassafel
+ */
+@RequestMapping(GroupController.MAPPING)
+public interface GroupController {
+    String MAPPING = "register";
+
+    @PostMapping
+    ResponseEntity<?> registerWithAccess(@Valid @RequestBody FullAccessGroup request);
+
+    @PutMapping
+    ResponseEntity<?> registerWithoutAccess(@Valid @RequestBody WithoutAccessGroup request);
+}
