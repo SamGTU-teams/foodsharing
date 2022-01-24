f3f0c2f26146e11c28709647c7f6139c80b8ea39
rassafel <rassafel@gmail.com>
2022-01-25 02:38:13 +0400

    Add mapper for geo object and test to them.

---
+++ parsers/vk-parser/impl/src/main/java/ru/rassafel/foodsharing/vkparser/model/mapper/GeoMapper.java
@@ -0,0 +1,27 @@
+package ru.rassafel.foodsharing.vkparser.model.mapper;
+
+import com.vk.api.sdk.objects.wall.Geo;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import ru.rassafel.foodsharing.common.model.GeoPoint;
+
+import java.util.Arrays;
+
+/**
+ * @author rassafel
+ */
+@Mapper(componentModel = "spring")
+public abstract class GeoMapper {
+    public static final GeoMapper INSTANCE = Mappers.getMapper(GeoMapper.class);
+
+    public GeoPoint apiToDto(Geo source) {
+        GeoPoint point = new GeoPoint();
+        double[] coordinates = Arrays.stream(source.getCoordinates().split("\\s+"))
+            .mapToDouble(Double::parseDouble)
+            .toArray();
+
+        point.setLat(coordinates[0]);
+        point.setLon(coordinates[1]);
+        return point;
+    }
+}
