332c79613ba47f9a264c0a98793757b78fe076bd
rassafel <rassafel@gmail.com>
2022-01-25 03:08:46 +0400

    Polish imports.

--- parsers/vk-parser/impl/src/test/java/ru/rassafel/foodsharing/vkparser/model/mapper/GeoMapperTest.java
+++ parsers/vk-parser/impl/src/test/java/ru/rassafel/foodsharing/vkparser/model/mapper/GeoMapperTest.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import ru.rassafel.foodsharing.common.model.GeoPoint;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author rassafel
