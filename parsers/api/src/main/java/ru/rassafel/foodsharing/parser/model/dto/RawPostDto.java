79a567ddf1858bbe30126a19850733b79594d77f
rassafel <rassafel@gmail.com>
2022-01-25 13:58:04 +0400

    Polish code.

--- parsers/api/src/main/java/ru/rassafel/foodsharing/parser/model/dto/RawPostDto.java
+++ parsers/api/src/main/java/ru/rassafel/foodsharing/parser/model/dto/RawPostDto.java
@@ -1,13 +1,8 @@
 package ru.rassafel.foodsharing.parser.model.dto;
 
 import lombok.Data;
-import ru.rassafel.foodsharing.common.model.GeoPoint;
-import ru.rassafel.foodsharing.common.model.dto.RegionDto;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * @author rassafel
