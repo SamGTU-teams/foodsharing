37283593211bb210bdd2e8a5901370a32eaaf3c9
rassafel <rassafel@gmail.com>
2022-01-27 20:41:32 +0400

    Change generated strategy to identity.

--- platform/common-db/src/main/java/ru/rassafel/foodsharing/common/model/entity/Product.java
+++ platform/common-db/src/main/java/ru/rassafel/foodsharing/common/model/entity/Product.java
@@ -17,7 +17,7 @@ import javax.persistence.*;
 @ToString
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
--- platform/common-db/src/main/java/ru/rassafel/foodsharing/common/model/entity/Region.java
+++ platform/common-db/src/main/java/ru/rassafel/foodsharing/common/model/entity/Region.java
@@ -18,7 +18,7 @@ import javax.persistence.*;
 @ToString
 public class Region {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
