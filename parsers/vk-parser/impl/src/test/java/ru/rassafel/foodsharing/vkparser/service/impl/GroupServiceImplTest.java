680fcac708b98b54629955b99038db8c13c0c3de
rassafel <rassafel@gmail.com>
2022-01-30 18:24:48 +0400

    Add mock for VkApiClient and test with full access registration.

--- parsers/vk-parser/impl/src/test/java/ru/rassafel/foodsharing/vkparser/service/impl/GroupServiceImplTest.java
+++ parsers/vk-parser/impl/src/test/java/ru/rassafel/foodsharing/vkparser/service/impl/GroupServiceImplTest.java
@@ -1,6 +1,5 @@
 package ru.rassafel.foodsharing.vkparser.service.impl;
 
-import com.vk.api.sdk.actions.Groups;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.GroupActor;
 import com.vk.api.sdk.exceptions.ApiException;
@@ -8,7 +7,6 @@ import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.base.responses.OkResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -21,7 +19,6 @@ import ru.rassafel.foodsharing.vkparser.model.entity.VkGroup;
 import ru.rassafel.foodsharing.vkparser.repository.GroupRepository;
 
 import java.util.Optional;
-import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -39,7 +36,7 @@ class GroupServiceImplTest {
     @Mock
     GroupRepository repository;
 
-    @Mock
+    @Mock(answer = Answers.RETURNS_DEEP_STUBS)
     VkApiClient client;
 
     @Mock
@@ -55,6 +52,7 @@ class GroupServiceImplTest {
 
         when(properties.getUrl()).thenReturn("TestUrl");
         when(properties.getServerTitle()).thenReturn("Test title");
+
         when(client.getVersion()).thenReturn("5.131");
 
         accepted = new VkGroup();
@@ -67,40 +65,65 @@ class GroupServiceImplTest {
     @ParameterizedTest
     @NullSource
     @ValueSource(strings = {"Test secret"})
-    void registerNewGroupWithAccess(String acceptedSecret) {
-        when(repository.findById(any()))
-            .thenReturn(Optional.empty());
-
-        accepted.setAccessToken("Test token");
-        accepted.setSecretKey(acceptedSecret);
-
-//        ToDo: Mock VkApiClient.
-
-        VkGroup actual = service.registerWithAccess(accepted);
-
-        verify(repository, times(1)).findById(eq(1));
-        verify(repository, times(2)).save(any());
+    void registerNewGroupWithAccess(String acceptedSecret) throws ClientException, ApiException {
+        registerGroupWithAccess(acceptedSecret, null);
     }
 
-    @Disabled
     @ParameterizedTest
     @MethodSource("ru.rassafel.foodsharing.vkparser.service.impl.SecretMatches#registerWithoutAccessMatchValues")
     void registerExistGroupWithAccess(String dbSecret, String acceptedSecret) throws ClientException, ApiException {
         fromDb.setAccessToken("Old token");
         fromDb.setSecretKey(dbSecret);
+        registerGroupWithAccess(acceptedSecret, fromDb);
+    }
 
-        when(repository.findById(any()))
-            .thenReturn(Optional.of(fromDb));
-
+    private void registerGroupWithAccess(String acceptedSecret, VkGroup fromDb) throws ApiException, ClientException {
         accepted.setAccessToken("Test token");
         accepted.setSecretKey(acceptedSecret);
 
-//        ToDo: Mock VkApiClient.
+        VkGroup expected = new VkGroup();
+        expected.setGroupId(1);
+        expected.setServerId(1);
+        expected.setAccessToken("Test token");
+        expected.setSecretKey(acceptedSecret);
+        expected.setConfirmationCode("Test code");
+
+        when(repository.findById(any()))
+            .thenReturn(Optional.ofNullable(fromDb));
+
+        when(client
+            .groups()
+            .getCallbackConfirmationCode(any(GroupActor.class), anyInt())
+            .execute()
+            .getCode()
+        ).thenReturn("Test code");
+
+        when(client
+            .groups()
+            .addCallbackServer(any(GroupActor.class), anyInt(), anyString(), anyString())
+            .secretKey(or(isNull(), anyString()))
+            .execute()
+            .getServerId()
+        ).thenReturn(1);
+
+        when(client
+            .groups()
+            .setCallbackSettings(any(GroupActor.class), anyInt())
+            .apiVersion(anyString())
+            .serverId(anyInt())
+            .wallPostNew(anyBoolean())
+            .execute()
+        ).thenReturn(OkResponse.OK);
 
         VkGroup actual = service.registerWithAccess(accepted);
 
         verify(repository, times(1)).findById(eq(1));
         verify(repository, times(2)).save(any());
+
+        assertThat(actual)
+            .isNotNull()
+            .isNotSameAs(expected)
+            .isEqualToComparingFieldByField(expected);
     }
 
     @ParameterizedTest
