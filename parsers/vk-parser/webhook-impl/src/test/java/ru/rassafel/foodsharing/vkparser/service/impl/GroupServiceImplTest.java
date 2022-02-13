ea3148867f170b2e9644aa291bdf832d0ec27498
rassafel <rassafel@gmail.com>
2022-02-13 13:00:27 +0400

    Polish tests.

--- parsers/vk-parser/webhook-impl/src/test/java/ru/rassafel/foodsharing/vkparser/service/impl/GroupServiceImplTest.java
+++ parsers/vk-parser/webhook-impl/src/test/java/ru/rassafel/foodsharing/vkparser/service/impl/GroupServiceImplTest.java
@@ -6,6 +6,7 @@ import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import com.vk.api.sdk.objects.base.responses.OkResponse;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
@@ -50,11 +51,6 @@ class GroupServiceImplTest {
     void setUp() {
         MockitoAnnotations.initMocks(this);
 
-        when(properties.getUrl()).thenReturn("TestUrl");
-        when(properties.getServerTitle()).thenReturn("Test title");
-
-        when(client.getVersion()).thenReturn("5.131");
-
         accepted = new VkGroup();
         accepted.setGroupId(1);
 
@@ -62,158 +58,173 @@ class GroupServiceImplTest {
         fromDb.setGroupId(1);
     }
 
-    @ParameterizedTest
-    @NullSource
-    @ValueSource(strings = {"Test secret"})
-    void registerNewGroupWithAccess(String acceptedSecret) throws ClientException, ApiException {
-        registerGroupWithAccess(acceptedSecret, null);
+
+    @Nested
+    class WithAccessCases {
+        @BeforeEach
+        void setUp() {
+            when(properties.getUrl()).thenReturn("TestUrl");
+            when(properties.getServerTitle()).thenReturn("Test title");
+
+            when(client.getVersion()).thenReturn("5.131");
+        }
+
+        @ParameterizedTest
+        @NullSource
+        @ValueSource(strings = {"Test secret"})
+        void registerNewGroupWithAccess(String acceptedSecret) throws ClientException, ApiException {
+            registerGroupWithAccess(acceptedSecret, null);
+        }
+
+        @ParameterizedTest
+        @MethodSource("ru.rassafel.foodsharing.vkparser.service.impl.SecretMatches#registerWithoutAccessMatchValues")
+        void registerExistGroupWithAccess(String dbSecret, String acceptedSecret) throws ClientException, ApiException {
+            fromDb.setAccessToken("Old token");
+            fromDb.setSecretKey(dbSecret);
+            registerGroupWithAccess(acceptedSecret, fromDb);
+        }
+
+        private void registerGroupWithAccess(String acceptedSecret, VkGroup fromDb) throws ApiException, ClientException {
+            accepted.setAccessToken("Test token");
+            accepted.setSecretKey(acceptedSecret);
+
+            VkGroup expected = new VkGroup();
+            expected.setGroupId(1);
+            expected.setServerId(1);
+            expected.setAccessToken("Test token");
+            expected.setSecretKey(acceptedSecret);
+            expected.setConfirmationCode("Test code");
+
+            when(repository.findById(any()))
+                .thenReturn(Optional.ofNullable(fromDb));
+
+            when(client
+                .groups()
+                .getCallbackConfirmationCode(any(GroupActor.class), anyInt())
+                .execute()
+                .getCode()
+            ).thenReturn("Test code");
+
+            when(client
+                .groups()
+                .addCallbackServer(any(GroupActor.class), anyInt(), anyString(), anyString())
+                .secretKey(or(isNull(), anyString()))
+                .execute()
+                .getServerId()
+            ).thenReturn(1);
+
+            when(client
+                .groups()
+                .setCallbackSettings(any(GroupActor.class), anyInt())
+                .apiVersion(anyString())
+                .serverId(anyInt())
+                .wallPostNew(anyBoolean())
+                .execute()
+            ).thenReturn(OkResponse.OK);
+
+            VkGroup actual = service.registerWithAccess(accepted);
+
+            verify(repository, times(1)).findById(eq(1));
+            verify(repository, times(1)).save(any());
+
+            assertThat(actual)
+                .isNotNull()
+                .isNotSameAs(expected)
+                .isEqualToComparingFieldByField(expected);
+        }
+
+        @ParameterizedTest
+        @MethodSource("ru.rassafel.foodsharing.vkparser.service.impl.SecretMatches#registerWithoutAccessNotMatchValues")
+        void registerExistGroupWithAccessSecretNotMatch(String dbSecret, String acceptedSecret) {
+            fromDb.setAccessToken("Old token");
+            fromDb.setSecretKey(dbSecret);
+
+            when(repository.findById(any()))
+                .thenReturn(Optional.of(fromDb));
+
+            accepted.setAccessToken("Test token");
+            accepted.setSecretKey(acceptedSecret);
+
+            assertThatThrownBy(() -> service.registerWithAccess(accepted))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("Secret key do not match.");
+
+            verify(repository, times(1)).findById(eq(1));
+            verify(repository, never()).save(any());
+        }
     }
 
-    @ParameterizedTest
-    @MethodSource("ru.rassafel.foodsharing.vkparser.service.impl.SecretMatches#registerWithoutAccessMatchValues")
-    void registerExistGroupWithAccess(String dbSecret, String acceptedSecret) throws ClientException, ApiException {
-        fromDb.setAccessToken("Old token");
-        fromDb.setSecretKey(dbSecret);
-        registerGroupWithAccess(acceptedSecret, fromDb);
-    }
+    @Nested
+    class WithoutAccessCases {
+        @ParameterizedTest
+        @NullSource
+        @ValueSource(strings = {"Test secret"})
+        void registerNewGroupWithoutAccess(String acceptedSecret) {
+            fromDb.setConfirmationCode("Test code");
+            fromDb.setSecretKey(acceptedSecret);
 
-    private void registerGroupWithAccess(String acceptedSecret, VkGroup fromDb) throws ApiException, ClientException {
-        accepted.setAccessToken("Test token");
-        accepted.setSecretKey(acceptedSecret);
+            when(repository.findById(any()))
+                .thenReturn(Optional.empty());
+            when(repository.save(any())).thenReturn(fromDb);
 
-        VkGroup expected = new VkGroup();
-        expected.setGroupId(1);
-        expected.setServerId(1);
-        expected.setAccessToken("Test token");
-        expected.setSecretKey(acceptedSecret);
-        expected.setConfirmationCode("Test code");
+            accepted.setConfirmationCode("Test code");
+            accepted.setSecretKey(acceptedSecret);
 
-        when(repository.findById(any()))
-            .thenReturn(Optional.ofNullable(fromDb));
+            VkGroup actual = service.registerWithoutAccess(accepted);
 
-        when(client
-            .groups()
-            .getCallbackConfirmationCode(any(GroupActor.class), anyInt())
-            .execute()
-            .getCode()
-        ).thenReturn("Test code");
+            verify(repository, times(1)).findById(eq(1));
+            verify(repository, times(1)).save(eq(accepted));
 
-        when(client
-            .groups()
-            .addCallbackServer(any(GroupActor.class), anyInt(), anyString(), anyString())
-            .secretKey(or(isNull(), anyString()))
-            .execute()
-            .getServerId()
-        ).thenReturn(1);
+            assertThat(actual)
+                .isNotNull()
+                .isSameAs(fromDb)
+                .isNotSameAs(accepted)
+                .isEqualToComparingFieldByField(accepted);
+        }
 
-        when(client
-            .groups()
-            .setCallbackSettings(any(GroupActor.class), anyInt())
-            .apiVersion(anyString())
-            .serverId(anyInt())
-            .wallPostNew(anyBoolean())
-            .execute()
-        ).thenReturn(OkResponse.OK);
+        @ParameterizedTest
+        @MethodSource("ru.rassafel.foodsharing.vkparser.service.impl.SecretMatches#registerWithoutAccessMatchValues")
+        void registerExistGroupWithoutAccess(String dbSecret, String acceptedSecret) {
+            fromDb.setConfirmationCode("Old code");
+            fromDb.setSecretKey(dbSecret);
 
-        VkGroup actual = service.registerWithAccess(accepted);
+            when(repository.findById(any()))
+                .thenReturn(Optional.of(fromDb));
+            when(repository.save(any())).thenReturn(fromDb);
 
-        verify(repository, times(1)).findById(eq(1));
-        verify(repository, times(2)).save(any());
+            accepted.setConfirmationCode("New code");
+            accepted.setSecretKey(acceptedSecret);
 
-        assertThat(actual)
-            .isNotNull()
-            .isNotSameAs(expected)
-            .isEqualToComparingFieldByField(expected);
-    }
+            VkGroup actual = service.registerWithoutAccess(accepted);
 
-    @ParameterizedTest
-    @MethodSource("ru.rassafel.foodsharing.vkparser.service.impl.SecretMatches#registerWithoutAccessNotMatchValues")
-    void registerExistGroupWithAccessSecretNotMatch(String dbSecret, String acceptedSecret) {
-        fromDb.setAccessToken("Old token");
-        fromDb.setSecretKey(dbSecret);
+            verify(repository, times(1)).findById(eq(1));
+            verify(repository, times(1)).save(eq(accepted));
 
-        when(repository.findById(any()))
-            .thenReturn(Optional.of(fromDb));
+            assertThat(actual)
+                .isNotNull()
+                .isSameAs(fromDb)
+                .isNotSameAs(accepted)
+                .isEqualToComparingFieldByField(accepted);
+        }
 
-        accepted.setAccessToken("Test token");
-        accepted.setSecretKey(acceptedSecret);
+        @ParameterizedTest
+        @MethodSource("ru.rassafel.foodsharing.vkparser.service.impl.SecretMatches#registerWithoutAccessNotMatchValues")
+        void registerExistGroupWithoutAccessSecretNotMatch(String dbSecret, String acceptedSecret) {
+            fromDb.setConfirmationCode("Old code");
+            fromDb.setSecretKey(dbSecret);
 
-        assertThatThrownBy(() -> service.registerWithAccess(accepted))
-            .isInstanceOf(RuntimeException.class)
-            .hasMessageContaining("Secret key do not match.");
+            when(repository.findById(any()))
+                .thenReturn(Optional.of(fromDb));
 
-        verify(repository, times(1)).findById(eq(1));
-        verify(repository, never()).save(any());
-    }
+            accepted.setConfirmationCode("New code");
+            accepted.setSecretKey(acceptedSecret);
 
-    @ParameterizedTest
-    @NullSource
-    @ValueSource(strings = {"Test secret"})
-    void registerNewGroupWithoutAccess(String acceptedSecret) {
-        fromDb.setConfirmationCode("Test code");
-        fromDb.setSecretKey(acceptedSecret);
+            assertThatThrownBy(() -> service.registerWithoutAccess(accepted))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessageContaining("Secret key do not match.");
 
-        when(repository.findById(any()))
-            .thenReturn(Optional.empty());
-        when(repository.save(any())).thenReturn(fromDb);
-
-        accepted.setConfirmationCode("Test code");
-        accepted.setSecretKey(acceptedSecret);
-
-        VkGroup actual = service.registerWithoutAccess(accepted);
-
-        verify(repository, times(1)).findById(eq(1));
-        verify(repository, times(1)).save(eq(accepted));
-
-        assertThat(actual)
-            .isNotNull()
-            .isSameAs(fromDb)
-            .isNotSameAs(accepted)
-            .isEqualToComparingFieldByField(accepted);
-    }
-
-    @ParameterizedTest
-    @MethodSource("ru.rassafel.foodsharing.vkparser.service.impl.SecretMatches#registerWithoutAccessMatchValues")
-    void registerExistGroupWithoutAccess(String dbSecret, String acceptedSecret) {
-        fromDb.setConfirmationCode("Old code");
-        fromDb.setSecretKey(dbSecret);
-
-        when(repository.findById(any()))
-            .thenReturn(Optional.of(fromDb));
-        when(repository.save(any())).thenReturn(fromDb);
-
-        accepted.setConfirmationCode("New code");
-        accepted.setSecretKey(acceptedSecret);
-
-        VkGroup actual = service.registerWithoutAccess(accepted);
-
-        verify(repository, times(1)).findById(eq(1));
-        verify(repository, times(1)).save(eq(accepted));
-
-        assertThat(actual)
-            .isNotNull()
-            .isSameAs(fromDb)
-            .isNotSameAs(accepted)
-            .isEqualToComparingFieldByField(accepted);
-    }
-
-    @ParameterizedTest
-    @MethodSource("ru.rassafel.foodsharing.vkparser.service.impl.SecretMatches#registerWithoutAccessNotMatchValues")
-    void registerExistGroupWithoutAccessSecretNotMatch(String dbSecret, String acceptedSecret) {
-        fromDb.setConfirmationCode("Old code");
-        fromDb.setSecretKey(dbSecret);
-
-        when(repository.findById(any()))
-            .thenReturn(Optional.of(fromDb));
-
-        accepted.setConfirmationCode("New code");
-        accepted.setSecretKey(acceptedSecret);
-
-        assertThatThrownBy(() -> service.registerWithoutAccess(accepted))
-            .isInstanceOf(RuntimeException.class)
-            .hasMessageContaining("Secret key do not match.");
-
-        verify(repository, times(1)).findById(eq(1));
-        verify(repository, never()).save(eq(accepted));
+            verify(repository, times(1)).findById(eq(1));
+            verify(repository, never()).save(eq(accepted));
+        }
     }
 }
