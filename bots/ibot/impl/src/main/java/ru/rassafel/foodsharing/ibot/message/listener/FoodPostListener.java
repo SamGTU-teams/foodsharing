eea79728cb4db7415077908633c632838d833171
rassafel <rassafel@gmail.com>
2022-02-15 21:39:37 +0400

    Update FoodPostListener.

--- bots/ibot/impl/src/main/java/ru/rassafel/foodsharing/ibot/message/listener/FoodPostListener.java
+++ bots/ibot/impl/src/main/java/ru/rassafel/foodsharing/ibot/message/listener/FoodPostListener.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import ru.rassafel.foodsharing.analyzer.model.dto.FoodPostDto;
 import ru.rassafel.foodsharing.bot.message.listener.MessageListener;
+import ru.rassafel.foodsharing.ibot.model.entity.FoodPost;
+import ru.rassafel.foodsharing.ibot.model.mapper.FoodPostMapper;
+import ru.rassafel.foodsharing.ibot.repository.FoodPostRepository;
 
 /**
  * @author rassafel
@@ -13,8 +16,15 @@ import ru.rassafel.foodsharing.bot.message.listener.MessageListener;
 @Slf4j
 @Component
 public class FoodPostListener implements MessageListener<FoodPostDto> {
+    private final FoodPostRepository repository;
+
+    private final FoodPostMapper mapper;
+
     @Override
-    public void receiveMessage(FoodPostDto message) {
-        log.info("Accepted message: {}", message);
+    public void receiveMessage(FoodPostDto foodPostDto) {
+        log.debug("Accepted message: {}", foodPostDto);
+
+        FoodPost foodPost = mapper.dtoToEntity(foodPostDto);
+        repository.save(foodPost);
     }
 }
