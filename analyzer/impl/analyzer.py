9e151ea6cc9d461f77060ab10f6de1644de1dede
rassafel <rassafel@gmail.com>
2022-02-16 12:04:36 +0400

    Add logging configuration.

---
+++ analyzer/impl/analyzer.py
@@ -0,0 +1,39 @@
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'disable_existing_loggers': True,
+
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s.%(msecs)03d %(levelname)5.5s %(process)d '
+                      '--- [%(threadName)15s] %(name)-40.40s: %(message)s',
+            'datefmt': '%Y-%m-%d,%H:%M:%S'
+        },
+    },
+
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'sqlalchemy': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        'sqlalchemy.engine.Engine': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False
+        }
+    }
+})
