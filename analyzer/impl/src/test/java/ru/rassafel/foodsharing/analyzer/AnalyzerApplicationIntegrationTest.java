63033ad0bcff8d0d6794ebd4688d4172e50a7d5b
rassafel <rassafel@gmail.com>
2022-03-07 20:37:42 +0400

    Add integration test class.

---
+++ analyzer/impl/src/test/java/ru/rassafel/foodsharing/analyzer/AnalyzerApplicationIntegrationTest.java
@@ -0,0 +1,18 @@
+package ru.rassafel.foodsharing.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author rassafel
+ */
+@SpringBootTest
+@ActiveProfiles("integration-test")
+class AnalyzerApplicationIntegrationTest {
+    @Test
+    void contextLoads() {
+    }
+}
