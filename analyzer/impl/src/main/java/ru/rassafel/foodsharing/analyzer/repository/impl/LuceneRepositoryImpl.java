95ab18e2a84a3671107040fe63ef63f06645f504
rassafel <rassafel@gmail.com>
2022-03-10 15:56:18 +0400

    Add LuceneRepository implementation and test to them.

---
+++ analyzer/impl/src/main/java/ru/rassafel/foodsharing/analyzer/repository/impl/LuceneRepositoryImpl.java
@@ -0,0 +1,120 @@
+package ru.rassafel.foodsharing.analyzer.repository.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+import ru.rassafel.foodsharing.analyzer.repository.LuceneRepository;
+import ru.rassafel.foodsharing.analyzer.model.LuceneObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author rassafel
+ */
+@RequiredArgsConstructor
+@Slf4j
+@Repository
+public class LuceneRepositoryImpl implements LuceneRepository {
+    public static final String FIELD_BODY = "body";
+    public static final String FIELD_ID = "id";
+    private final SearcherManager searcherManager;
+    private final IndexWriter writer;
+    @Value("${lucene.numberPerPage}")
+    private final int numberPerPage;
+
+    @Override
+    public LuceneObject create(String text) {
+        String hash = DigestUtils.md5Hex(text).toUpperCase();
+        LuceneObject object = new LuceneObject(hash, text);
+        create(object);
+        return object;
+    }
+
+    private void create(LuceneObject... objects) {
+        List<Document> docs = Arrays.stream(objects).map(this::map)
+            .collect(Collectors.toUnmodifiableList());
+        try {
+            writer.addDocuments(docs);
+        } catch (IOException e) {
+            log.error("Lucene index writer IO exception.", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void delete(String id) {
+        Term term = new Term(FIELD_ID, id);
+        try {
+            writer.deleteDocuments(term);
+        } catch (IOException e) {
+            log.error("Lucene index writer IO exception.", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<LuceneObject> search(Query query, int number) {
+        try {
+            searcherManager.maybeRefresh();
+            IndexSearcher searcher = searcherManager.acquire();
+            TopDocs result = searcher.search(query, number);
+            return map(searcher, result);
+        } catch (IOException e) {
+            log.error("Lucene searcher IO exception.", e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public List<LuceneObject> search(Query query) {
+        return search(query, numberPerPage);
+    }
+
+    @Override
+    public List<LuceneObject> findAll() {
+        MatchAllDocsQuery query = new MatchAllDocsQuery();
+        return search(query);
+    }
+
+    @Override
+    public List<LuceneObject> findAll(int count) {
+        MatchAllDocsQuery query = new MatchAllDocsQuery();
+        return search(query, count);
+    }
+
+    List<LuceneObject> map(IndexSearcher searcher, TopDocs topDocs) throws IOException {
+        List<LuceneObject> result = new ArrayList<>(topDocs.scoreDocs.length);
+        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+            Document doc = searcher.doc(scoreDoc.doc);
+            LuceneObject object = map(doc);
+            result.add(object);
+        }
+        return result;
+    }
+
+    LuceneObject map(Document doc) {
+        String id = doc.getField(FIELD_ID).stringValue();
+        String body = doc.getField(FIELD_BODY).stringValue();
+        return new LuceneObject(id, body);
+    }
+
+    Document map(LuceneObject object) {
+        Document doc = new Document();
+        doc.add(new StringField(FIELD_ID, object.getId(), Field.Store.YES));
+        doc.add(new TextField(FIELD_BODY, object.getBody(), Field.Store.YES));
+        return doc;
+    }
+}
---
+++ analyzer/impl/src/test/java/ru/rassafel/foodsharing/analyzer/repository/impl/LuceneRepositoryImplTest.java
@@ -0,0 +1,107 @@
+package ru.rassafel.foodsharing.analyzer.repository.impl;
+
+import lombok.SneakyThrows;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.*;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.shaded.org.apache.commons.lang.RandomStringUtils;
+import ru.rassafel.foodsharing.analyzer.model.LuceneObject;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author rassafel
+ */
+class LuceneRepositoryImplTest {
+    LuceneRepositoryImpl repository;
+    Directory directory;
+    SearcherManager searcherManager;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        directory = new RAMDirectory();
+        StandardAnalyzer analyzer = new StandardAnalyzer();
+        IndexWriterConfig config = new IndexWriterConfig(analyzer);
+        IndexWriter writer = new IndexWriter(directory, config);
+        searcherManager = new SearcherManager(writer, false, false, null);
+        repository = new LuceneRepositoryImpl(searcherManager, writer, 100);
+    }
+
+    @SneakyThrows
+    LuceneObject createFirstObject(String body) {
+        assertThat(repository.findAll())
+            .isEmpty();
+
+        LuceneObject result = repository.create(body);
+
+        assertThat(result)
+            .isNotNull();
+        assertThat(result.getBody())
+            .isEqualTo(body);
+        assertThat(result.getId())
+            .isNotEmpty();
+
+        assertThat(repository.findAll())
+            .isNotEmpty();
+
+        return result;
+    }
+
+    @Test
+    void create() throws IOException {
+        String body = "Test value for create object.";
+        LuceneObject object = createFirstObject(body);
+    }
+
+    @Test
+    void delete() throws IOException {
+        String body = "Test value for create object.";
+        LuceneObject object = createFirstObject(body);
+        repository.delete(object);
+        assertThat(repository.findAll())
+            .isEmpty();
+    }
+
+    @Test
+    void search() {
+        String body = "Test value for create object.";
+        LuceneObject object = createFirstObject(body);
+        Stream.generate(() -> RandomStringUtils.randomAlphabetic(16))
+            .limit(10)
+            .forEach(text -> repository.create(text));
+
+        TermQuery query = new TermQuery(new Term(LuceneRepositoryImpl.FIELD_ID, object.getId()));
+        List<LuceneObject> result = repository.search(query);
+
+        assertThat(result)
+            .hasSize(1);
+
+        LuceneObject actual = result.get(0);
+
+        assertThat(actual)
+            .isNotNull();
+        assertThat(actual.getId())
+            .isEqualTo(object.getId());
+    }
+
+    @Test
+    void findAll() {
+        int count = 10;
+        Stream.generate(() -> RandomStringUtils.randomAlphabetic(16))
+            .limit(count)
+            .forEach(text -> repository.create(text));
+        List<LuceneObject> actual = repository.findAll(count);
+        assertThat(actual)
+            .hasSize(count);
+    }
+}
