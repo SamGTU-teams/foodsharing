7c8979a49e92eebe90bb6303d1b54c488536a029
rassafel <rassafel@gmail.com>
2022-03-30 14:52:11 +0400

    Add query returns Stream.

--- analyzer/impl/src/main/java/ru/rassafel/foodsharing/analyzer/repository/ProductRepository.java
+++ analyzer/impl/src/main/java/ru/rassafel/foodsharing/analyzer/repository/ProductRepository.java
@@ -1,10 +1,13 @@
 package ru.rassafel.foodsharing.analyzer.repository;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import ru.rassafel.foodsharing.common.model.entity.Product;
+
+import java.util.stream.Stream;
 
 /**
  * @author rassafel
  */
-public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
+public interface ProductRepository extends CrudRepository<Product, Long> {
+    Stream<Product> findByNameIsNotNull();
 }
