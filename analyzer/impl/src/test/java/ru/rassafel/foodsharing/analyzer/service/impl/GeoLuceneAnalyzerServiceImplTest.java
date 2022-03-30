4b530ba9099c431e441cd805255fd2d29b74ac92
rassafel <rassafel@gmail.com>
2022-03-30 18:24:12 +0400

    Add test for GeoLuceneAnalyzerServiceImpl.

---
+++ analyzer/impl/src/test/java/ru/rassafel/foodsharing/analyzer/service/impl/GeoLuceneAnalyzerServiceImplTest.java
@@ -0,0 +1,53 @@
+package ru.rassafel.foodsharing.analyzer.service.impl;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.rassafel.foodsharing.common.model.GeoPoint;
+import ru.rassafel.foodsharing.parser.model.PostContext;
+import ru.rassafel.foodsharing.parser.model.RawPost;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author rassafel
+ */
+class GeoLuceneAnalyzerServiceImplTest {
+    GeoLuceneAnalyzerServiceImpl service;
+
+    @BeforeEach
+    void setUp() {
+        service = new GeoLuceneAnalyzerServiceImpl(null);
+    }
+
+    @Test
+    void parseGeoPoint() {
+        double lat = 10;
+        double lon = 20;
+
+        GeoPoint geoPoint = new GeoPoint(lat, lon);
+        GeoPoint expected = new GeoPoint(lat, lon);
+
+        PostContext context = new PostContext();
+        context.setAttachments(List.of());
+        context.setPoint(geoPoint);
+
+        RawPost post = new RawPost();
+        post.setDate(LocalDateTime.now());
+        post.setText("Test string");
+        post.setUrl("test.url");
+        post.setContext(context);
+
+        Optional<GeoPoint> actual = service.parseGeoPoint(post);
+
+        assertThat(actual)
+            .isNotEmpty();
+
+        assertThat(actual.get())
+            .isNotSameAs(expected)
+            .isEqualTo(expected);
+    }
+}
