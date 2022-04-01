49b32e21e0012c5fcc7c6bd652134c508f353bad
rassafel <rassafel@gmail.com>
2022-04-01 17:05:03 +0400

    Change Stream to Streamable.

--- analyzer/impl/src/main/java/ru/rassafel/foodsharing/analyzer/service/ProductLuceneAnalyzerService.java
+++ analyzer/impl/src/main/java/ru/rassafel/foodsharing/analyzer/service/ProductLuceneAnalyzerService.java
@@ -1,17 +1,16 @@
 package ru.rassafel.foodsharing.analyzer.service;
 
 import org.springframework.data.util.Pair;
+import org.springframework.data.util.Streamable;
 import ru.rassafel.foodsharing.analyzer.model.LuceneIndexedString;
 import ru.rassafel.foodsharing.common.model.entity.Product;
 import ru.rassafel.foodsharing.parser.model.RawPost;
-
-import java.util.stream.Stream;
 
 /**
  * @author rassafel
  */
 public interface ProductLuceneAnalyzerService extends ProductAnalyzerService {
-    Stream<Pair<Product, Float>> parseProducts(RawPost post, LuceneIndexedString... indexedStrings);
+    Streamable<Pair<Product, Float>> parseProducts(RawPost post, LuceneIndexedString... indexedStrings);
 
-    Stream<Pair<Product, Float>> parseProducts(LuceneIndexedString... indexedStrings);
+    Streamable<Pair<Product, Float>> parseProducts(LuceneIndexedString... indexedStrings);
 }
