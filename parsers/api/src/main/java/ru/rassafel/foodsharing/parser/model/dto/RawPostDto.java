da5f3e16e34e2e097a56259d71946671b5e5336f
rassafel <rassafel@gmail.com>
2022-05-09 22:35:28 +0400

    Add validation.

--- parsers/api/src/main/java/ru/rassafel/foodsharing/parser/model/dto/RawPostDto.java
+++ parsers/api/src/main/java/ru/rassafel/foodsharing/parser/model/dto/RawPostDto.java
@@ -1,18 +1,25 @@
 package ru.rassafel.foodsharing.parser.model.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import ru.rassafel.foodsharing.parser.model.PostContext;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
 
 /**
  * @author rassafel
  */
 @Data
-public class RawPost {
-    @NotBlank
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class RawPostDto {
+    //    Nullable and NotBlack
+    @Pattern(regexp = "^(?!\\s*$).+")
     private String url;
 
     @NotNull
