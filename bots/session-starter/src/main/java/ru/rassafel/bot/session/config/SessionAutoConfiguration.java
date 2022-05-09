088e6a872b52ae98a10a43fa704671de5ad2a5fd
rassafel <rassafel@gmail.com>
2022-05-09 22:40:44 +0400

    Move configurations.

--- bots/session-starter/src/main/java/ru/rassafel/bot/session/config/SessionAutoConfiguration.java
+++ bots/session-starter/src/main/java/ru/rassafel/bot/session/config/SessionAutoConfiguration.java
@@ -1,21 +1,57 @@
 package ru.rassafel.bot.session.config;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.mustachejava.DefaultMustacheFactory;
+import com.github.mustachejava.MustacheFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.client.RestTemplate;
+import ru.rassafel.bot.session.model.entity.Place;
 
 /**
  * @author rassafel
  */
 @Configuration
-@ConditionalOnProperty(name = "bot.session.enabled", matchIfMissing = true)
+@ConditionalOnProperty(prefix = "bot.session", name = "enabled", matchIfMissing = true)
 @EnableConfigurationProperties(SessionProperties.class)
 //Скан всех компонентов модуля
 @ComponentScan(basePackages = "ru.rassafel.bot.session")
 @EnableJpaRepositories(basePackages = "ru.rassafel.bot.session.repository")
 @EntityScan(basePackages = "ru.rassafel.bot.session.model.entity")
 public class SessionAutoConfiguration {
+    @Bean
+    public MessageSource botMessagesSource() {
+        ReloadableResourceBundleMessageSource messageSource =
+            new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplateBuilder()
+            .build();
+    }
+
+    @Bean
+    public MustacheFactory mustacheFactory() {
+        return new DefaultMustacheFactory();
+    }
+
+    @Bean
+    public Cache<Long, Place> geoPointCache(SessionProperties properties) {
+        return Caffeine.newBuilder()
+            .expireAfterWrite(properties.getPlacesCache().getExpirationTime())
+            .build();
+    }
 }
