11d2c7cb1bc7324bafd9d1f884b952809353aa58
rassafel <rassafel@gmail.com>
2022-05-09 23:18:43 +0400

    Add RabbitMQ listener for AD posts in VK and TG bots.

---
+++ bots/tg-bot/src/main/java/ru/rassafel/foodsharing/tgbot/message/AdPostRabbitListener.java
@@ -0,0 +1,36 @@
+package ru.rassafel.foodsharing.tgbot.message;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import ru.rassafel.foodsharing.ad.model.AdPostDto;
+import ru.rassafel.foodsharing.tgbot.repository.TgUserRepository;
+import ru.rassafel.foodsharing.tgbot.service.TgBotHandlerService;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Validator;
+import java.util.Set;
+
+/**
+ * @author rassafel
+ */
+@RequiredArgsConstructor
+@Slf4j
+@Component
+@RabbitListener(queues = {"spring.rabbit.ad-post.queue"})
+public class AdPostRabbitListener {
+    private final Validator validator;
+
+    @RabbitHandler
+    public void receiveMessage(AdPostDto adPostDto) {
+        Set<ConstraintViolation<AdPostDto>> violations = validator.validate(adPostDto);
+        if (!violations.isEmpty()) {
+            log.debug("Validation exception", new ConstraintViolationException(violations));
+            return;
+        }
+//        ToDo: Send message for all users
+    }
+}
