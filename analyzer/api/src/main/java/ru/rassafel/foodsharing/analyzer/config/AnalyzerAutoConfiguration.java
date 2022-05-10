d08d0afc504321745ac2e055eee224be5caf8727
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-10 21:59:14 +0400

    Add enable feign to AnalyzerAutoConfiguration

--- analyzer/api/src/main/java/ru/rassafel/foodsharing/analyzer/config/AnalyzerAutoConfiguration.java
+++ analyzer/api/src/main/java/ru/rassafel/foodsharing/analyzer/config/AnalyzerAutoConfiguration.java
@@ -1,6 +1,7 @@
 package ru.rassafel.foodsharing.analyzer.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -8,5 +9,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @ConditionalOnProperty(name = {"feign.analyzer.url"})
+@EnableFeignClients
 public class AnalyzerAutoConfiguration {
 }
