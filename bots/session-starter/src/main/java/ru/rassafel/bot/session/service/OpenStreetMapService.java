a15e062a5328bb6d1fe79c12da87ce5cd3b2eb56
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-12 23:01:01 +0400

    Fix and finished OpenStreetMapService

--- bots/session-starter/src/main/java/ru/rassafel/bot/session/service/OpenStreetMapService.java
+++ bots/session-starter/src/main/java/ru/rassafel/bot/session/service/OpenStreetMapService.java
@@ -1,21 +1,23 @@
 package ru.rassafel.bot.session.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.geojson.Feature;
 import org.geojson.FeatureCollection;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.Optional.ofNullable;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OpenStreetMapService {
     private static final String OPEN_MAP_ROOT_PATH =
         "https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}&format={format}&accept-language={lang}";
@@ -28,30 +30,38 @@ public class OpenStreetMapService {
             lon,
             "geojson",
             "ru");
+
+        String resultName = "Не определено";
+        JsonNode root;
         try {
-            FeatureCollection allFeatures = objectMapper.readValue(response, FeatureCollection.class);
-            List<Feature> features = allFeatures.getFeatures();
-            if (features.isEmpty()) {
-                return "Место не распознано";
-            }
-            Feature feature = features.get(0);
-            Map<String, Object> properties = feature.getProperties();
-            if (properties.isEmpty()) {
-                return "Место не распознано";
-            }
-
-            Map<String, String> address = ofNullable(properties.get("address"))
-                .map(map -> (Map<String, String>) map)
-                .orElseThrow();
-
-            List<String> addressParts = new LinkedList<>();
-
-
+             root = objectMapper.readTree(response);
         } catch (JsonProcessingException e) {
-            e.printStackTrace();
+            log.warn("Place name not found by coordinates : {}, {}", lat, lon);
+            return resultName;
         }
-
-        return response;
+        JsonNode features = root.get("features");
+        if(features == null || !features.isArray()){
+            log.warn("Place name not found by coordinates : {}, {}", lat, lon);
+            return resultName;
+        }
+        if(features.isEmpty()){
+            return resultName;
+        }
+        JsonNode firstFeature = features.get(0);
+        if(firstFeature != null){
+            JsonNode properties = firstFeature.get("properties");
+            if(properties != null){
+                JsonNode name = properties.get("display_name2");
+                if(name == null){
+                    return resultName;
+                }
+                List<String> splitted = Arrays.stream(name.asText().split(","))
+                    .map(String::trim).collect(Collectors.toList());
+                Collections.reverse(splitted);
+                resultName = String.join(", ", splitted);
+            }
+        }
+        return resultName;
     }
 
 }
