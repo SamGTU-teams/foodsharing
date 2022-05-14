b7a4ec0763d5d711b59550093facdde531d790bc
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-14 17:31:05 +0400

    Add method for find data by foodPost to User service

--- bots/session-starter/src/main/java/ru/rassafel/bot/session/service/UserService.java
+++ bots/session-starter/src/main/java/ru/rassafel/bot/session/service/UserService.java
@@ -1,11 +1,15 @@
 package ru.rassafel.bot.session.service;
 
 import ru.rassafel.bot.session.model.entity.User;
+import ru.rassafel.foodsharing.analyzer.model.dto.FoodPostDto;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserService {
     void saveUser(User user);
 
     Optional<? extends User> getUser(Long id);
+
+    List<Object[]> findByFoodPost(FoodPostDto post);
 }
