f76b28d98e23701e134b7fb2e0f36cbe340d2605
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-14 17:38:34 +0400

    Fix vk food post listener

--- bots/vk-bot/src/main/java/ru/rassafel/foodsharing/vkbot/message/FoodPostRabbitListener.java
+++ bots/vk-bot/src/main/java/ru/rassafel/foodsharing/vkbot/message/FoodPostRabbitListener.java
@@ -1,21 +1,23 @@
 package ru.rassafel.foodsharing.vkbot.message;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+import ru.rassafel.bot.session.service.FoodPostHandlerService;
 import ru.rassafel.foodsharing.analyzer.model.dto.FoodPostDto;
 import ru.rassafel.foodsharing.common.model.GeoPoint;
 import ru.rassafel.foodsharing.common.model.dto.ProductDto;
 import ru.rassafel.foodsharing.vkbot.repository.VkUserRepository;
-import ru.rassafel.foodsharing.vkbot.service.VkMessageSchedulerService;
+import ru.rassafel.foodsharing.vkbot.service.VkMessenger;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
-import java.util.List;
-import java.util.Set;
+import java.math.BigInteger;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -23,9 +25,9 @@ import java.util.stream.Collectors;
 @Component
 @RabbitListener(queues = {"${spring.rabbitmq.ready-post.queue}"})
 public class FoodPostRabbitListener {
-    private final VkUserRepository repository;
-    private final VkMessageSchedulerService schedulerService;
+
     private final Validator validator;
+    private final FoodPostHandlerService foodPostHandlerService;
 
     @RabbitHandler
     public void receiveMessage(FoodPostDto foodPostDto) {
@@ -34,20 +36,18 @@ public class FoodPostRabbitListener {
             log.debug("Validation exception", new ConstraintViolationException(violations));
             return;
         }
+        foodPostHandlerService.handleFoodPostReceived(foodPostDto);
+    }
 
-        GeoPoint point = foodPostDto.getPoint();
-        double lat = point.getLat();
-        double lon = point.getLon();
-        List<Long> productIds = foodPostDto.getProducts()
-            .stream()
-            .map(ProductDto::getId)
-            .collect(Collectors.toList());
-        List<Long> userIds = repository.findByProductAndSuitablePlace(productIds, lat, lon)
-            .stream()
-            .map(o -> (Long) o[0])
-            .collect(Collectors.toList());
-        String postText = foodPostDto.getText();
-        schedulerService.scheduleEvent(postText, userIds.stream()
-            .map(Long::intValue).toArray(Integer[]::new));
+    @Getter
+    private static class ProductsPlacesEntry {
+        private final Set<String> productNames = new HashSet<>();
+        private final Set<String> placesNames = new HashSet<>();
+
+        public void addEntry(String productName, String placeName){
+            productNames.add(productName);
+            placesNames.add(placeName);
+        }
+
     }
 }
