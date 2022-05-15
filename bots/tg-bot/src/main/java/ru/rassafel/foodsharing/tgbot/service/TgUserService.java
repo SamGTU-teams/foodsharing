1feeff7031c72b4484f4243e77e58cd0da1a21c0
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-15 21:08:03 +0400

    Add getByFoodPost method

--- bots/tg-bot/src/main/java/ru/rassafel/foodsharing/tgbot/service/TgUserService.java
+++ bots/tg-bot/src/main/java/ru/rassafel/foodsharing/tgbot/service/TgUserService.java
@@ -4,11 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.rassafel.bot.session.model.entity.User;
 import ru.rassafel.bot.session.service.UserService;
+import ru.rassafel.foodsharing.analyzer.model.dto.FoodPostDto;
+import ru.rassafel.foodsharing.common.model.GeoPoint;
+import ru.rassafel.foodsharing.common.model.dto.ProductDto;
 import ru.rassafel.foodsharing.tgbot.model.TgUser;
 import ru.rassafel.foodsharing.tgbot.repository.TgUserRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,5 +31,12 @@ public class TgUserService implements UserService {
     @Override
     public Optional<User> getUser(Long id) {
         return repository.findById(id).map(Function.identity());
+    }
+
+    @Override
+    public List<Object[]> findByFoodPost(FoodPostDto post) {
+        GeoPoint point = post.getPoint();
+        List<Long> collect = post.getProducts().stream().map(ProductDto::getId).collect(Collectors.toList());
+        return repository.findByProductAndSuitablePlace(collect, point.getLat(), point.getLon());
     }
 }
