7b201ae133655597fdd529df3e1b89e3e8fbde64
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-15 21:44:33 +0400

    Add stub product controller

--- analyzer/api/src/main/java/ru/rassafel/foodsharing/analyzer/config/AnalyzerAutoConfiguration.java
+++ analyzer/api/src/main/java/ru/rassafel/foodsharing/analyzer/config/AnalyzerAutoConfiguration.java
@@ -2,7 +2,10 @@ package ru.rassafel.foodsharing.analyzer.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import ru.rassafel.foodsharing.analyzer.controller.stub.ProductAnalyzerControllerStub;
 
 /**
  * @author rassafel
@@ -11,4 +14,12 @@ import org.springframework.context.annotation.Configuration;
 @ConditionalOnProperty(name = {"feign.analyzer.url"})
 @EnableFeignClients
 public class AnalyzerAutoConfiguration {
+
+    @Bean
+    @ConditionalOnProperty(value = "product-analyzer.stub-mode", havingValue = "true")
+    @Primary
+    public ProductAnalyzerControllerStub productAnalyzerControllerStub(){
+        return new ProductAnalyzerControllerStub();
+    }
+
 }
---
+++ analyzer/api/src/main/java/ru/rassafel/foodsharing/analyzer/controller/stub/ProductAnalyzerControllerStub.java
@@ -0,0 +1,18 @@
+package ru.rassafel.foodsharing.analyzer.controller.stub;
+
+import ru.rassafel.foodsharing.analyzer.controller.ProductAnalyzerController;
+import ru.rassafel.foodsharing.analyzer.model.dto.ScoreProductDto;
+import ru.rassafel.foodsharing.common.model.dto.ProductDto;
+
+import java.util.List;
+
+public class ProductAnalyzerControllerStub implements ProductAnalyzerController {
+    @Override
+    public List<ScoreProductDto> parseProducts(String text, Long count) {
+        return List.of(
+            new ScoreProductDto(new ProductDto(1L, "Молоко"), 1F),
+            new ScoreProductDto(new ProductDto(2L, "Ряженка"), 0.9F),
+            new ScoreProductDto(new ProductDto(3L, "Кефир"), 0.8F)
+        );
+    }
+}
