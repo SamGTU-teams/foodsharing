3e144ce5f81375836ea220eca9ba18248e8a2f89
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-15 19:36:36 +0400

    Remove unused config

--- bots/tg-bot/src/main/java/ru/rassafel/foodsharing/tgbot/config/TgBotConfiguration.java
+++ bots/tg-bot/src/main/java/ru/rassafel/foodsharing/tgbot/config/TgBotConfiguration.java
@@ -1,18 +1,17 @@
 package ru.rassafel.foodsharing.tgbot.config;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.meta.ApiContext;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import ru.rassafel.foodsharing.tgbot.mapper.TgBotDtoMapper;
 import ru.rassafel.foodsharing.tgbot.service.TgBotHandlerService;
 
 @Configuration
 @EnableConfigurationProperties(TgBotProperties.class)
 @EntityScan(basePackages = "ru.rassafel.foodsharing.tgbot.model")
+@EnableScheduling
 public class TgBotConfiguration {
     @Bean
     TgBotDtoMapper tgBotDtoMapper() {
@@ -21,10 +20,8 @@ public class TgBotConfiguration {
 
     @Bean
     public TgBotHandlerService foodSharingWebHookBot(TgBotProperties properties) {
-        DefaultBotOptions options = ApiContext.getInstance(DefaultBotOptions.class);
-        options.setProxyHost("localhost");
-        options.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
-        options.setProxyPort(9150);
-        return new TgBotHandlerService(properties.getBotUsername(), properties.getToken(), properties.getWebHookPath());
+        return new TgBotHandlerService(properties.getBotUsername(),
+            properties.getToken(),
+            properties.getWebHookPath());
     }
 }
