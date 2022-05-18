b6406c51ee42dd9c49114172e8bac83d79ebab60
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-18 17:31:15 +0400

    Add mustache templates call

--- bots/session-starter/src/main/java/ru/rassafel/bot/session/step/geo/StartGeoStep.java
+++ bots/session-starter/src/main/java/ru/rassafel/bot/session/step/geo/StartGeoStep.java
@@ -8,18 +8,21 @@ import ru.rassafel.bot.session.model.dto.SessionResponse;
 import ru.rassafel.bot.session.model.entity.EmbeddedUserSession;
 import ru.rassafel.bot.session.model.entity.User;
 import ru.rassafel.bot.session.service.UserService;
+import ru.rassafel.bot.session.service.message.TemplateEngine;
 import ru.rassafel.bot.session.step.Step;
+import ru.rassafel.bot.session.templates.PlaceTemplates;
 import ru.rassafel.bot.session.util.GeoButtonsUtil;
 
 @Component("geo-0")
 @RequiredArgsConstructor
 public class StartGeoStep implements Step {
     private final UserService userService;
+    private final TemplateEngine templateEngine;
 
     @Override
     public void executeStep(SessionRequest sessionRequest, SessionResponse sessionResponse, User user) {
         EmbeddedUserSession userSession = user.getUserSession();
-        sessionResponse.setMessage("Выберите:");
+        sessionResponse.setMessage(templateEngine.compileTemplate(PlaceTemplates.CHOOSE_GEO_OPERATION));
         BotButtons responseButtons = new BotButtons();
         sessionResponse.setButtons(responseButtons.addAll(GeoButtonsUtil.GEO_MAIN_BUTTONS));
         userSession.setSessionStep(ChooseOperationGeoStep.STEP_INDEX);
--- bots/session-starter/src/main/java/ru/rassafel/bot/session/templates/PlaceTemplates.java
+++ bots/session-starter/src/main/java/ru/rassafel/bot/session/templates/PlaceTemplates.java
@@ -4,7 +4,8 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum PlaceTemplates implements Templates {
-    ;
+    CHOOSE_GEO_OPERATION("places/choose-geo-operation"),
+    EXPECTATION_OF_GEO("places/expectation-of-geo");
 
     private final String name;
 
