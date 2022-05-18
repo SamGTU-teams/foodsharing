d5fd6bcfc9325b3b9939f95bccf8a9a328d0c9b8
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-18 17:18:58 +0400

    Add mustache templates call

--- bots/session-starter/src/main/java/ru/rassafel/bot/session/step/product/StartProductStep.java
+++ bots/session-starter/src/main/java/ru/rassafel/bot/session/step/product/StartProductStep.java
@@ -8,7 +8,9 @@ import ru.rassafel.bot.session.model.dto.SessionResponse;
 import ru.rassafel.bot.session.model.entity.EmbeddedUserSession;
 import ru.rassafel.bot.session.model.entity.User;
 import ru.rassafel.bot.session.service.UserService;
+import ru.rassafel.bot.session.service.message.TemplateEngine;
 import ru.rassafel.bot.session.step.Step;
+import ru.rassafel.bot.session.templates.ProductTemplates;
 
 import static ru.rassafel.bot.session.util.ProductButtonsUtil.PRODUCT_MAIN_BUTTONS;
 
@@ -16,11 +18,12 @@ import static ru.rassafel.bot.session.util.ProductButtonsUtil.PRODUCT_MAIN_BUTTO
 @RequiredArgsConstructor
 public class StartProductStep implements Step {
     private final UserService userService;
+    private final TemplateEngine templateEngine;
 
     @Override
     public void executeStep(SessionRequest sessionRequest, SessionResponse sessionResponse, User user) {
         EmbeddedUserSession userSession = user.getUserSession();
-        sessionResponse.setMessage("Выберите:");
+        sessionResponse.setMessage(templateEngine.compileTemplate(ProductTemplates.CHOOSE_PRODUCT_OPERATION));
         BotButtons responseButtons = new BotButtons();
         sessionResponse.setButtons(responseButtons.addAll(PRODUCT_MAIN_BUTTONS));
         userSession.setSessionStep(ChooseOperationProductStep.STEP_INDEX);
