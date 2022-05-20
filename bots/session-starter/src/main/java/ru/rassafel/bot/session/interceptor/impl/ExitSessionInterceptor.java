fb5f01879d0422032179ca923a530b8be8b3a937
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-20 14:22:40 +0400

    Add mustache calls to ExitSessionInterceptor and delete file props service

--- bots/session-starter/src/main/java/ru/rassafel/bot/session/interceptor/impl/ExitSessionInterceptor.java
+++ bots/session-starter/src/main/java/ru/rassafel/bot/session/interceptor/impl/ExitSessionInterceptor.java
@@ -11,30 +11,31 @@ import ru.rassafel.bot.session.model.dto.To;
 import ru.rassafel.bot.session.model.entity.EmbeddedUserSession;
 import ru.rassafel.bot.session.model.entity.Place;
 import ru.rassafel.bot.session.model.entity.User;
-import ru.rassafel.bot.session.service.FilePropertiesService;
 import ru.rassafel.bot.session.service.UserService;
+import ru.rassafel.bot.session.service.message.TemplateEngine;
+import ru.rassafel.bot.session.templates.MainTemplates;
 import ru.rassafel.bot.session.type.BotSession;
 import ru.rassafel.bot.session.util.ButtonsUtil;
 
 @Component
 @RequiredArgsConstructor
 public class ExitSessionInterceptor implements SessionInterceptor {
-    private final FilePropertiesService filePropertiesService;
     private final Cache<Long, Place> geoPointCache;
     private final UserService userService;
+    private final TemplateEngine templateEngine;
 
     @Override
     public SessionResponse handle(SessionRequest request, User user, BotSession next) {
         final String userMsg = request.getMessage();
 
         EmbeddedUserSession userSession = user.getUserSession();
-        if (userMsg.equals(filePropertiesService.getButtonName("back-to-main")) && userSession != null) {
+        if (userMsg != null && userMsg.equalsIgnoreCase("На главную") && userSession != null) {
             geoPointCache.invalidate(user.getId());
 
             userSession.setSessionActive(false);
             userService.saveUser(user);
             return SessionResponse.builder()
-                .message(filePropertiesService.getSessionMessage("back-to-main"))
+                .message(templateEngine.compileTemplate(MainTemplates.BACK_TO_MAIN))
                 .sendTo(new To(user.getId()))
                 .buttons(new BotButtons(ButtonsUtil.DEFAULT_BUTTONS))
                 .build();
