9fd44430f0b21f963678b4ff7e9a4d19c1f972ce
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-22 21:08:29 +0400

    Add mustache engine call

--- bots/session-starter/src/main/java/ru/rassafel/bot/session/step/geo/SetRadiusAndFinishSaveGeoStep.java
+++ bots/session-starter/src/main/java/ru/rassafel/bot/session/step/geo/SetRadiusAndFinishSaveGeoStep.java
@@ -15,7 +15,10 @@ import ru.rassafel.bot.session.model.entity.Place;
 import ru.rassafel.bot.session.model.entity.User;
 import ru.rassafel.bot.session.service.PlaceService;
 import ru.rassafel.bot.session.service.UserService;
+import ru.rassafel.bot.session.service.message.TemplateEngine;
 import ru.rassafel.bot.session.step.Step;
+import ru.rassafel.bot.session.templates.MainTemplates;
+import ru.rassafel.bot.session.templates.PlaceTemplates;
 
 import static ru.rassafel.bot.session.util.ButtonsUtil.DEFAULT_BUTTONS;
 import static ru.rassafel.bot.session.util.GeoButtonsUtil.GEO_MAIN_BUTTONS;
@@ -31,6 +34,8 @@ public class SetRadiusAndFinishSaveGeoStep implements Step {
     private final UserService userService;
     private final PlatformTransactionManager transactionManager;
 
+    private final TemplateEngine templateEngine;
+
     @Override
     public void executeStep(SessionRequest sessionRequest, SessionResponse sessionResponse, User user) {
         EmbeddedUserSession userSession = user.getUserSession();
@@ -41,7 +46,7 @@ public class SetRadiusAndFinishSaveGeoStep implements Step {
         if (place == null) {
             sessionResponse.setButtons(new BotButtons(DEFAULT_BUTTONS));
             userSession.setSessionActive(false);
-            sessionResponse.setMessage("Время данной операции истекло");
+            sessionResponse.setMessage(templateEngine.compileTemplate(MainTemplates.OPERATION_TIMEOUT));
             userService.saveUser(user);
             return;
         }
@@ -55,11 +60,12 @@ public class SetRadiusAndFinishSaveGeoStep implements Step {
         } else {
             try {
                 radius = Integer.parseInt(message);
-                if (radius >= 5000) {
-                    throw new BotException(user.getId(), "Нужно ввести радиус меньше 5 км");
+                if (radius >= 5000 || radius <= 0) {
+                    throw new BotException(user.getId(), templateEngine.compileTemplate(PlaceTemplates.INVALID_RADIUS_RANGE));
                 }
             } catch (NumberFormatException ex) {
-                throw new BotException(user.getId(), "Введите число");
+                throw new BotException(user.getId(),
+                    templateEngine.compileTemplate(PlaceTemplates.INVALID_RADIUS_FORMAT));
             }
         }
 
@@ -75,7 +81,7 @@ public class SetRadiusAndFinishSaveGeoStep implements Step {
             userService.saveUser(user);
         });
 
-        sessionResponse.setMessage("Круто! Место сохранено");
+        sessionResponse.setMessage(templateEngine.compileTemplate(PlaceTemplates.FINISHED_SAVE_POINT));
 
         sessionResponse.setButtons(responseButtons.addAll(GEO_MAIN_BUTTONS));
     }
