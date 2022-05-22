587e281922381c1c9ecd48bbc1c91ba7fe899fbc
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-22 22:14:55 +0400

    Ad mustache template engine call

--- bots/session-starter/src/main/java/ru/rassafel/bot/session/type/impl/WelcomeSession.java
+++ bots/session-starter/src/main/java/ru/rassafel/bot/session/type/impl/WelcomeSession.java
@@ -1,14 +1,14 @@
 package ru.rassafel.bot.session.type.impl;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.rassafel.bot.session.model.dto.BotButtons;
 import ru.rassafel.bot.session.model.dto.SessionRequest;
 import ru.rassafel.bot.session.model.dto.SessionResponse;
 import ru.rassafel.bot.session.model.dto.To;
 import ru.rassafel.bot.session.model.entity.User;
-import ru.rassafel.bot.session.service.FilePropertiesService;
+import ru.rassafel.bot.session.service.message.TemplateEngine;
+import ru.rassafel.bot.session.templates.MainTemplates;
 import ru.rassafel.bot.session.type.BotSession;
 import ru.rassafel.bot.session.util.ButtonsUtil;
 
@@ -16,15 +16,14 @@ import ru.rassafel.bot.session.util.ButtonsUtil;
 @Component(WelcomeSession.NAME)
 public class WelcomeSession implements BotSession {
     public static final String NAME = "welcomeSession";
-    @Autowired
-    private FilePropertiesService propertiesService;
+    private final TemplateEngine templateEngine;
 
     @Override
     public SessionResponse execute(SessionRequest request, User user) {
         return SessionResponse
             .builder()
             .buttons(new BotButtons(ButtonsUtil.DEFAULT_BUTTONS))
-            .message(propertiesService.getSessionMessage("welcome"))
+            .message(templateEngine.compileTemplate(MainTemplates.WELCOME, null))
             .sendTo(To.builder()
                 .id(user.getId())
                 .build())
