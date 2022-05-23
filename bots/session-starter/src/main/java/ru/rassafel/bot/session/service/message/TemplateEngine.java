34d39a24ece01a71453a494c1fbda37471b5c664
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-23 15:00:10 +0400

    Add methods to TemplateEngine

--- bots/session-starter/src/main/java/ru/rassafel/bot/session/service/message/TemplateEngine.java
+++ bots/session-starter/src/main/java/ru/rassafel/bot/session/service/message/TemplateEngine.java
@@ -1,8 +1,12 @@
 package ru.rassafel.bot.session.service.message;
 
+import ru.rassafel.bot.session.templates.Templates;
+
 /**
  * @author rassafel
  */
 public interface TemplateEngine {
-    String compileTemplate(String template, Object context);
+    String compileTemplate(Templates template, Object context);
+    String compileTemplate(Templates template);
+
 }
