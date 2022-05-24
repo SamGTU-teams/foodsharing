3931f678900f037af1653a1480fcb23913575dbe
rassafel <rassafel@gmail.com>
2022-05-24 18:32:16 +0400

    Add EnableFeignClients annotation.

--- bots/ibot/api/src/main/java/ru/rassafel/foodsharing/ibot/config/IBotAutoConfiguration.java
+++ bots/ibot/api/src/main/java/ru/rassafel/foodsharing/ibot/config/IBotAutoConfiguration.java
@@ -1,12 +1,14 @@
 package ru.rassafel.foodsharing.ibot.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author rassafel
  */
 @Configuration
+@EnableFeignClients
 @EnableConfigurationProperties(IBotProperties.class)
 public class IBotAutoConfiguration {
 }
