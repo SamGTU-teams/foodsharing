2120fb2563933abc079ae330b279d6008ffd6de4
rassafel <rassafel@gmail.com>
2022-05-24 18:31:57 +0400

    Move mapping UserService findByFoodPost to implementations.

--- bots/session-starter/src/main/java/ru/rassafel/bot/session/service/FoodPostHandlerService.java
+++ bots/session-starter/src/main/java/ru/rassafel/bot/session/service/FoodPostHandlerService.java
@@ -6,57 +6,25 @@ import ru.rassafel.bot.session.service.message.TemplateEngine;
 import ru.rassafel.bot.session.templates.MainTemplates;
 import ru.rassafel.foodsharing.analyzer.model.dto.FoodPostDto;
 
-import java.math.BigInteger;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class FoodPostHandlerService {
-
     private final UserService userService;
     private final Messenger messenger;
     private final TemplateEngine templateEngine;
 
-    public void handleFoodPostReceived(FoodPostDto foodPostDto){
-
-        class ProductsPlacesEntry {
-            private final Set<String> productNames = new HashSet<>();
-            private final Set<String> placesNames = new HashSet<>();
-
-            public void addEntry(String productName, String placeName){
-                productNames.add(productName);
-                placesNames.add(placeName);
-            }
-        }
-
-        List<Object[]> data = userService.findByFoodPost(foodPostDto);
-
-        Map<Long, ProductsPlacesEntry> grouped = data.stream().collect(Collectors.toMap(
-            array -> ((BigInteger) array[0]).longValue(),
-            array -> {
-                ProductsPlacesEntry entry = new ProductsPlacesEntry();
-                entry.addEntry((String) array[2], (String) array[1]);
-                return entry;
-            },
-            (oldEntry, newEntry) -> {
-                oldEntry.placesNames.addAll(newEntry.placesNames);
-                oldEntry.productNames.addAll(newEntry.productNames);
-                return oldEntry;
-            }));
-        grouped.forEach((key, value) -> {
+    public void handleFoodPostReceived(FoodPostDto foodPostDto) {
+        userService.findByFoodPost(foodPostDto).forEach(result -> {
             String resultMessage = templateEngine.compileTemplate(MainTemplates.POST_INFO,
                 Map.of(
-                    "places", value.placesNames,
-                    "products", value.productNames,
+                    "places", result.getPlaceNames(),
+                    "products", result.getProductNames(),
                     "url", foodPostDto.getUrl(),
                     "text", foodPostDto.getText()
                 ));
-            messenger.send(resultMessage, key.intValue());
+            messenger.send(resultMessage, result.getUserId().intValue());
         });
     }
-
 }
--- bots/tg-bot/src/main/java/ru/rassafel/foodsharing/tgbot/service/TgUserService.java
+++ bots/tg-bot/src/main/java/ru/rassafel/foodsharing/tgbot/service/TgUserService.java
@@ -1,6 +1,7 @@
 package ru.rassafel.foodsharing.tgbot.service;
 
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Service;
 import ru.rassafel.bot.session.model.entity.User;
 import ru.rassafel.bot.session.service.UserService;
@@ -10,9 +11,10 @@ import ru.rassafel.foodsharing.common.model.dto.ProductDto;
 import ru.rassafel.foodsharing.tgbot.model.TgUser;
 import ru.rassafel.foodsharing.tgbot.repository.TgUserRepository;
 
+import java.math.BigInteger;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -34,10 +36,29 @@ public class TgUserService implements UserService {
     }
 
     @Override
-    public List<Object[]> findByFoodPost(FoodPostDto post) {
+    public List<UserPlacesAndProducts> findByFoodPost(FoodPostDto post) {
         GeoPoint point = post.getPoint();
-        List<Long> collect = post.getProducts().stream().map(ProductDto::getId).collect(Collectors.toList());
-        return repository.findByProductAndSuitablePlace(collect, point.getLat(), point.getLon());
+        List<Long> products = post.getProducts().stream()
+            .map(ProductDto::getId)
+            .collect(Collectors.toList());
+
+        return repository.findByProductAndSuitablePlace(products, point.getLat(), point.getLon()).
+            stream().collect(Collectors.toMap(
+                array -> ((BigInteger) array[0]).longValue(),
+                array -> {
+                    Pair<HashSet<String>, HashSet<String>> pair
+                        = Pair.of(new HashSet<>(), new HashSet<>());
+                    pair.getLeft().add((String) array[1]); // place
+                    pair.getRight().add((String) array[2]); // product
+                    return pair;
+                },
+                (oldEntry, newEntry) -> {
+                    oldEntry.getRight().addAll(newEntry.getRight());
+                    oldEntry.getLeft().addAll(newEntry.getLeft());
+                    return oldEntry;
+                })).entrySet().stream()
+            .map(entry -> new UserPlacesAndProducts(entry.getKey(), entry.getValue().getLeft(), entry.getValue().getRight()))
+            .collect(Collectors.toList());
     }
 
     @Override
