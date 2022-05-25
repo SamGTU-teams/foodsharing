ed158e4d2c5092ea66fcd822959029370b79b8e9
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-25 21:42:47 +0400

    Delete vk queue

--- bots/vk-bot/src/main/java/ru/rassafel/foodsharing/vkbot/config/VkBotConfiguration.java
+++ bots/vk-bot/src/main/java/ru/rassafel/foodsharing/vkbot/config/VkBotConfiguration.java
@@ -1,7 +1,6 @@
 package ru.rassafel.foodsharing.vkbot.config;
 
 import com.google.gson.GsonBuilder;
-import com.vk.api.sdk.client.AbstractQueryBuilder;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.GroupActor;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
@@ -10,13 +9,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import ru.rassafel.foodsharing.vkbot.model.mapper.VkBotDtoMapper;
-
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
 
 /**
  * @author rassafel
@@ -47,18 +41,5 @@ public class VkBotConfiguration {
     @Bean
     public GroupActor groupActor(VkBotProperties properties) {
         return new GroupActor(properties.getGroupId(), properties.getAccessToken());
-    }
-
-    @Bean
-    public BlockingQueue<AbstractQueryBuilder> vkQueryQueue(VkBotProperties properties) {
-        return new ArrayBlockingQueue<>(properties.getClient().getMaxRequestQueueSize());
-    }
-
-    @Bean
-    public TaskScheduler sendVkQueryTaskScheduler(VkBotProperties properties) {
-        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
-        threadPoolTaskScheduler.setPoolSize(properties.getClient().getMaxThreadCountForSendQueries());
-        threadPoolTaskScheduler.setThreadNamePrefix("SendVkQueryTaskScheduler-");
-        return threadPoolTaskScheduler;
     }
 }
