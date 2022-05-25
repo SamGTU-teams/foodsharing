1e2bd7ed6e560992459fab6ffcc1e454bbd2dd52
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-25 21:41:00 +0400

    Add abstract queue messenger

--- bots/vk-bot/src/main/java/ru/rassafel/foodsharing/vkbot/service/VkBotHandlerService.java
+++ bots/vk-bot/src/main/java/ru/rassafel/foodsharing/vkbot/service/VkBotHandlerService.java
@@ -8,9 +8,14 @@ import org.springframework.stereotype.Service;
 import ru.rassafel.foodsharing.session.exception.BotException;
 import ru.rassafel.foodsharing.session.model.dto.SessionRequest;
 import ru.rassafel.foodsharing.session.model.dto.SessionResponse;
+import ru.rassafel.foodsharing.session.model.dto.To;
+import ru.rassafel.foodsharing.session.service.message.TemplateEngine;
 import ru.rassafel.foodsharing.session.service.session.SessionService;
+import ru.rassafel.foodsharing.session.templates.MainTemplates;
 import ru.rassafel.foodsharing.vkbot.model.dto.VkUpdate;
 import ru.rassafel.foodsharing.vkbot.model.mapper.VkBotDtoMapper;
+
+import java.util.concurrent.BlockingQueue;
 
 @Service
 @Slf4j
@@ -18,22 +23,34 @@ import ru.rassafel.foodsharing.vkbot.model.mapper.VkBotDtoMapper;
 public class VkBotHandlerService {
     private final VkBotDtoMapper mapper;
     private final SessionService sessionService;
-    private final VkMessenger vkMessenger;
     @Value("${vk.bot.confirm_code}")
     private String confirmCode;
+    private final BlockingQueue<SessionResponse> queue;
+    private final TemplateEngine templateEngine;
 
     public String handleUpdate(VkUpdate update) {
         if (update.getType() == Type.MESSAGE_NEW) {
             SessionRequest request = mapper.mapDto(update);
+            SessionResponse response;
             try {
-                SessionResponse response = sessionService.handle(request);
-                vkMessenger.send(response);
+                response = sessionService.handle(request);
             } catch (BotException ex) {
-                vkMessenger.send(ex.getMessage(), ex.getSendTo().intValue());
+                response = SessionResponse.builder()
+                    .sendTo(new To(ex.getSendTo()))
+                    .message(ex.getMessage())
+                    .build();
             } catch (Exception ex) {
-                log.error("Caught an error {}", ex.getMessage());
-                vkMessenger.send("Возникла ошибка на сервере, попробуйте повторить позже",
-                    request.getFrom().getId().intValue());
+                log.error("Caught an error while  {}", ex.getMessage());
+                response = SessionResponse.builder()
+                    .sendTo(new To(request.getFrom().getId()))
+                    .message(templateEngine.compileTemplate(MainTemplates.ERROR_ON_SERVER))
+                    .build();
+            }
+            try {
+                queue.put(response);
+            } catch (InterruptedException e) {
+                log.error("Exception while put response to queue with message : {}",
+                    e.getMessage());
             }
         } else if (update.getType() == Type.CONFIRMATION) {
             return confirmCode;
