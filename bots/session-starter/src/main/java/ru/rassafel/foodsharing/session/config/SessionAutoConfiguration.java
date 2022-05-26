7daf6621e74ebce396cf9b8c0c37cb5ad4c6b8bf
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-26 19:36:29 +0400

    Little fixes

--- bots/session-starter/src/main/java/ru/rassafel/foodsharing/session/config/SessionAutoConfiguration.java
+++ bots/session-starter/src/main/java/ru/rassafel/foodsharing/session/config/SessionAutoConfiguration.java
@@ -4,26 +4,27 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Repository;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.client.RestTemplate;
 import ru.rassafel.foodsharing.session.model.dto.SessionResponse;
 import ru.rassafel.foodsharing.session.model.entity.Place;
 import ru.rassafel.foodsharing.session.repository.CallbackLockRepository;
 
-import static java.util.Objects.nonNull;
-
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+
+import static java.util.Objects.nonNull;
 
 /**
  * @author rassafel
@@ -53,6 +54,19 @@ public class SessionAutoConfiguration {
             .build();
     }
 
+    @Bean
+    public BlockingQueue<SessionResponse> queryQueue() {
+        return new ArrayBlockingQueue<>(properties.getMessenger().getMaxRequestQueueSize());
+    }
+
+    @Bean
+    public TaskScheduler sendQueryTaskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(properties.getMessenger().getMaxThreadCountForSendQueries());
+        threadPoolTaskScheduler.setThreadNamePrefix("SendQueryTaskScheduler-");
+        return threadPoolTaskScheduler;
+    }
+
     @Configuration
     @ConditionalOnMissingBean(CallbackLockRepository.class)
     protected static class DefaultCallbackLockRepositoryConfiguration {
@@ -63,8 +77,12 @@ public class SessionAutoConfiguration {
                 .build();
         }
 
+        @Bean
+        public DefaultCallbackLockRepository defaultCallbackLockRepository(Cache<Long, MutablePair<Long, Boolean>> callbackCache){
+            return new DefaultCallbackLockRepository(callbackCache);
+        }
+
         @RequiredArgsConstructor
-        @Repository
         public static class DefaultCallbackLockRepository implements CallbackLockRepository {
             private final Cache<Long, MutablePair<Long, Boolean>> callbackCache;
 
@@ -105,18 +123,5 @@ public class SessionAutoConfiguration {
                 return callbackCache.getIfPresent(userId);
             }
         }
-    }
-
-    @Bean
-    public BlockingQueue<SessionResponse> queryQueue() {
-        return new ArrayBlockingQueue<>(properties.getMessenger().getMaxRequestQueueSize());
-    }
-
-    @Bean
-    public TaskScheduler sendQueryTaskScheduler() {
-        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
-        threadPoolTaskScheduler.setPoolSize(properties.getMessenger().getMaxThreadCountForSendQueries());
-        threadPoolTaskScheduler.setThreadNamePrefix("SendQueryTaskScheduler-");
-        return threadPoolTaskScheduler;
     }
 }
--- bots/session-starter/src/main/java/ru/rassafel/foodsharing/session/config/SessionProperties.java
+++ bots/session-starter/src/main/java/ru/rassafel/foodsharing/session/config/SessionProperties.java
@@ -1,6 +1,8 @@
 package ru.rassafel.foodsharing.session.config;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.time.Duration;
