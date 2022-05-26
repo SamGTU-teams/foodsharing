506d6ae03aa88f2760abbe97eb7815750fa17a75
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-26 11:45:59 +0400

    Fix all messages

--- bots/session-starter/src/main/java/ru/rassafel/foodsharing/session/util/GeoButtonsUtil.java
+++ bots/session-starter/src/main/java/ru/rassafel/foodsharing/session/util/GeoButtonsUtil.java
@@ -8,14 +8,14 @@ public interface GeoButtonsUtil {
         MY_PLACES = "Мои места",
         ADD_PLACE = "Добавить место",
         DELETE_PLACE = "Удалить место",
-        EDIT_PLACE = "Редактирование места",
+        EDIT_PLACE = "Редактировать место",
         DELETE_ALL = "Удалить все",
         LEAVE_RADIUS_AS_IS = "Оставить как есть";
 
     List<String> GEO_MAIN_BUTTONS = List.of(
         MY_PLACES,
         ADD_PLACE,
-        DELETE_PLACE,
-        EDIT_PLACE
+        EDIT_PLACE,
+        DELETE_PLACE
     );
 }
