5336243abec7fef649d71d81a7fdda61d2f8b3dd
rassafel <rassafel@gmail.com>
2022-05-26 22:24:48 +0400

    Add Caffeine dependency and update properties.

--- parsers/vk-parser/webhook-impl/src/main/java/ru/rassafel/foodsharing/vkparser/config/VkParserProperties.java
+++ parsers/vk-parser/webhook-impl/src/main/java/ru/rassafel/foodsharing/vkparser/config/VkParserProperties.java
@@ -1,7 +1,12 @@
 package ru.rassafel.foodsharing.vkparser.config;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 
 /**
  * @author rassafel
@@ -12,12 +17,26 @@ public class VkParserProperties {
     public static final String PREFIX = "vk.parser";
 
     private String callbackUrl;
-
     private String serverTitle;
+    private Client client = new Client();
+    private CallbackProperties callback = new CallbackProperties();
 
-    private int retryAttemptsInternalServerErrorCount = 3;
+    @Data
+    public static class Client {
+        private int retryAttemptsInternalServerErrorCount = 3;
+        private int retryAttemptsNetworkErrorCount = 3;
+        private int retryAttemptsInvalidStatusCount = 3;
+    }
 
-    private int retryAttemptsNetworkErrorCount = 3;
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Cache {
+        private Duration expirationTime = Duration.of(10, ChronoUnit.MINUTES);
+    }
 
-    private int retryAttemptsInvalidStatusCount = 3;
+    @Data
+    public static class CallbackProperties {
+        private Cache cache = new Cache(Duration.of(2, ChronoUnit.DAYS));
+    }
 }
