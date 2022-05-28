72010d09a8c17ba2c3cbfb1ee18160c0e8dff02f
rassafel <rassafel@gmail.com>
2022-05-28 16:40:35 +0400

    Add stub matching in Feign clients.

--- bots/ibot/api/src/main/java/ru/rassafel/foodsharing/ibot/controller/feign/IBotControllerFeign.java
+++ bots/ibot/api/src/main/java/ru/rassafel/foodsharing/ibot/controller/feign/IBotControllerFeign.java
@@ -1,5 +1,6 @@
 package ru.rassafel.foodsharing.ibot.controller.feign;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.rassafel.foodsharing.ibot.controller.IBotController;
 
@@ -7,5 +8,6 @@ import ru.rassafel.foodsharing.ibot.controller.IBotController;
  * @author rassafel
  */
 @FeignClient(name = "ibot", url = "${feign.ibot.url}")
+@ConditionalOnProperty(name = "ibot.stub-mode", havingValue = "false")
 public interface IBotControllerFeign extends IBotController {
 }
