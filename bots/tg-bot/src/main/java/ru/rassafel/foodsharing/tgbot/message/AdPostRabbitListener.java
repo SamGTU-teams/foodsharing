83aa29ac3b9a03bb7da457f4999dc491f0945f24
Your Name <roma.grigoriev.2015@inbox.ru>
2022-05-28 15:38:36 +0400

    Fix fixed 2 listeners

--- bots/tg-bot/src/main/java/ru/rassafel/foodsharing/tgbot/message/AdPostRabbitListener.java
+++ bots/tg-bot/src/main/java/ru/rassafel/foodsharing/tgbot/message/AdPostRabbitListener.java
@@ -4,13 +4,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 import ru.rassafel.foodsharing.ad.model.AdPostDto;
+import ru.rassafel.foodsharing.session.model.dto.SessionResponse;
+import ru.rassafel.foodsharing.session.model.dto.To;
+import ru.rassafel.foodsharing.tgbot.repository.TgUserRepository;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
 import java.util.Set;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * @author rassafel
@@ -21,6 +27,9 @@ import java.util.Set;
 @RabbitListener(queues = {"${spring.rabbitmq.ad-post.queue}"})
 public class AdPostRabbitListener {
     private final Validator validator;
+    private final BlockingQueue<SessionResponse> queue;
+    private final TgUserRepository tgUserRepository;
+    private static final int PAGE_SIZE = 50;
 
     @RabbitHandler
     public void receiveMessage(AdPostDto adPostDto) {
@@ -29,6 +38,21 @@ public class AdPostRabbitListener {
             log.debug("Validation exception", new ConstraintViolationException(violations));
             return;
         }
-//        ToDo: Send message for all users
+        int pageNum = 0;
+        Page<Long> userIds;
+        while ((userIds = tgUserRepository.findAllUserIds(PageRequest.of(pageNum++, PAGE_SIZE))).hasContent()) {
+            userIds.stream().map(id ->
+                    SessionResponse.builder()
+                        .sendTo(new To(id))
+                        .message(adPostDto.getMessage())
+                        .build())
+                .forEach(sr -> {
+                    try {
+                        queue.put(sr);
+                    } catch (InterruptedException e) {
+                        log.error("Caught an exception while put ad message to queue");
+                    }
+                });
+        }
     }
 }
--- bots/vk-bot/src/main/java/ru/rassafel/foodsharing/vkbot/message/AdPostRabbitListener.java
+++ bots/vk-bot/src/main/java/ru/rassafel/foodsharing/vkbot/message/AdPostRabbitListener.java
@@ -4,13 +4,19 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 import ru.rassafel.foodsharing.ad.model.AdPostDto;
+import ru.rassafel.foodsharing.session.model.dto.SessionResponse;
+import ru.rassafel.foodsharing.session.model.dto.To;
+import ru.rassafel.foodsharing.vkbot.repository.VkUserRepository;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.Validator;
 import java.util.Set;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * @author rassafel
@@ -20,7 +26,10 @@ import java.util.Set;
 @Component
 @RabbitListener(queues = {"${spring.rabbitmq.ad-post.queue}"})
 public class AdPostRabbitListener {
+    private static final int PAGE_SIZE = 100;
     private final Validator validator;
+    private final VkUserRepository vkUserRepository;
+    private final BlockingQueue<SessionResponse> queue;
 
     @RabbitHandler
     public void receiveMessage(AdPostDto adPostDto) {
@@ -29,6 +38,17 @@ public class AdPostRabbitListener {
             log.debug("Validation exception", new ConstraintViolationException(violations));
             return;
         }
-//        ToDo: Send message for all users
+        int pageNum = 0;
+        Page<Long> userIds;
+        while ((userIds = vkUserRepository.findAllUserIds(PageRequest.of(pageNum++, PAGE_SIZE))).hasContent()) {
+            try {
+                queue.put(SessionResponse.builder()
+                        .sendTo(new To(userIds.getContent()))
+                        .message(adPostDto.getMessage())
+                    .build());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
